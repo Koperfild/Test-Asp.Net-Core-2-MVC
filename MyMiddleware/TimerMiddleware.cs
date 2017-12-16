5ac7a539de3e367c8a1ce6309ce2014f627ac9b8
Koperfild <Avalancher2@rambler.ru>
2017-12-16 02:01:52 +0200

    First commit

---
+++ MyMiddleware/TimerMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace TestMiddleware.MyMiddleware
+{
+    public class TimerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TimerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, TimeService timeService)
+        {
+            if (context.Request.Path.Value.ToLower() == "/time")
+            {
+                context.Response.ContentType = "text/html; charset=utf-8";
+                await context.Response.WriteAsync($"Текущее время: {timeService?.Time}");
+                //await _next.Invoke(context);
+                Thread.Sleep(2000);
+                await context.Response.WriteAsync($"Текущее время: {timeService?.Time}");
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
